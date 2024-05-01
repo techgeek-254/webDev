c0bc79edce49f82cb276a4a2d8a4dd5d3d8b8e25
John Mutua <johnstone.m10@gmail.com>
2024-05-02 00:18:25 +0300

    javascript beginner

---
+++ javaScriptCourse/index.js
@@ -0,0 +1,11 @@
+//console.log(`Hello John`);
+let name="John";
+let age = 34;
+
+/*console.log(typeof age);
+console.log(typeof name);
+console.log(`Welcome ${name}, we see you are ${age} years old !`); */
+
+//DOM
+document.getElementById("myH1").innerHTML= `Welcome ${name} `;
+document.getElementById("myP").textContent="Lorem ipsum"
