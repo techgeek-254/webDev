a1242d5d13eee3bcfae3283cfc0bae0872cd8ccb
John Mutua <johnstone.m10@gmail.com>
2024-05-06 17:22:53 +0300

    boolean js

--- javaScriptPractice2/index.js
+++ javaScriptPractice2/index.js
@@ -8,4 +8,9 @@ console.log(`Welcome ${name}, we see you are ${age} years old !`); */
 
 //DOM
 document.getElementById("myH1").innerHTML = `${course} in ${language} `;
-document.getElementById("myP").textContent = "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
+document.getElementById("myP").textContent = "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum";
+
+//boolean expression (True or False)
+let isStudent = false;
+console.log(`This is a student: ${isStudent}`);
+
