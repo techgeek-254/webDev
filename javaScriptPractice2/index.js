7732c22931df177224d5f6c0eb71cdbd27edd65c
John Mutua <johnstone.m10@gmail.com>
2024-05-06 16:43:17 +0300

    variable edits

--- javaScriptPractice2/index.js
+++ javaScriptPractice2/index.js
@@ -1,11 +1,11 @@
 //console.log(`Hello John`);
-let name="John";
-let age = 34;
+let course = "Programming";
+let language = "JavaScript";
 
 /*console.log(typeof age);
 console.log(typeof name);
 console.log(`Welcome ${name}, we see you are ${age} years old !`); */
 
 //DOM
-document.getElementById("myH1").innerHTML= `Welcome ${name} `;
-document.getElementById("myP").textContent="Lorem ipsum"
+document.getElementById("myH1").innerHTML = `${course} in ${language} `;
+document.getElementById("myP").textContent = "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
