ccd253ef162b66a8069d18ad2eeacef956efe81c
John Mutua <johnstone.m10@gmail.com>
2024-05-15 17:54:41 +0300

    label renamed

--- javaScriptPractice2/index.js
+++ javaScriptPractice2/index.js
@@ -1,5 +1,5 @@
 //console.log(`Hello John`);
-let course = "Programming";
+let course = "";
 let language = "JavaScript";
 
 /*console.log(typeof age);
@@ -7,7 +7,7 @@ console.log(typeof name);
 console.log(`Welcome ${name}, we see you are ${age} years old !`); */
 
 //DOM
-document.getElementById("myH1").innerHTML = `${course} in ${language} `;
+document.getElementById("myH1").innerHTML = `${course} ${language} `;
 document.getElementById("myP").textContent = "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum";
 
 //boolean expression (True or False)
