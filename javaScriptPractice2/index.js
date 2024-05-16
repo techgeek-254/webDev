bae1cf465607919590ec464279b4a9a3a3d91c5a
John Mutua <johnstone.m10@gmail.com>
2024-05-16 19:03:26 +0300

    end of switches

--- javaScriptPractice2/index.js
+++ javaScriptPractice2/index.js
@@ -156,35 +156,38 @@ console.log(theMessage);
 
 let days = 7;
 
-switch (days) {
-    case 1:
-        console.log("It is Monday");
-        break;
+    switch (days) {
+        case 1:
+            console.log("It is Monday");
+            break;
+    
+        case 2:
+            console.log("It is Tuesday");
+            break;
+    
+        case 3:
+            console.log("It is Wednesday");
+            break;
+    
+        case 4:
+            console.log("It is Thursday");
+            break;
+    
+        case 5:
+            console.log("It is Friday");
+            break;
+    
+        case 6:
+            console.log("It is Saturday");
+            break;
+    
+        case 7:
+            console.log("It is Sunday");
+            break;
+    
+        default:
+            console.log(`${days} is not a day of the week`);
+    }
 
-    case 2:
-        console.log("It is Tuesday");
-        break;
+/*******************************************************************************************/
 
-    case 3:
-        console.log("It is Wednesday");
-        break;
-
-    case 4:
-        console.log("It is Thursday");
-        break;
-
-    case 5:
-        console.log("It is Friday");
-        break;
-
-    case 6:
-        console.log("It is Saturday");
-        break;
-
-    case 7:
-        console.log("It is Sunday");
-        break;
-
-    default:
-        console.log(`${days} is not a day of the week`);
-}
