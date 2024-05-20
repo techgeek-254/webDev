e459650b2c4e2c1de4e8423db5f90968b3b1e55d
John Mutua <johnstone.m10@gmail.com>
2024-05-20 17:41:08 +0300

    change

--- javaScriptPractice2/index.js
+++ javaScriptPractice2/index.js
@@ -40,8 +40,9 @@ document.getElementById("mySubmit").onclick = function () {
 const PI = 3.14159;
 let radius;
 let circumference;
+let mySubmit = document.getElementById("mySubmit");
 
-document.getElementById("mySubmit").onclick = function () {
+mySubmit.onclick = function () {
     radius = document.getElementById("myInput").value;
     radius = Number(radius);
     circumference = 2 * PI * radius;
