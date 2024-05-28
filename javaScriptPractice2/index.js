f4a1e10272daab036fbc95094b19ad073e2a99f7
John Mutua <johnstone.m10@gmail.com>
2024-05-28 18:33:48 +0300

    Constructors

--- javaScriptPractice2/index.js
+++ javaScriptPractice2/index.js
@@ -419,3 +419,22 @@ const person2 = {
     p2Alert: function () {console.log(`Hi, I am ${this.p2Name}`)},
 }
 person2.p2Alert();
+
+/*********************************************************************** */
+
+ // CONSTRUCTORS = special method for defining the properties and methods of objects
+
+ function Car (make, model, year, color){
+    this.make = make,
+    this.model = model,
+    this.year = year,
+    this.color = color
+ }
+
+ const car1 = new Car("Ford", "Mustang", 2024, "Red");
+
+
+ console.log(car1.make);
+ console.log(car1.model);
+ console.log(car1.year);
+ console.log(car1.color);
