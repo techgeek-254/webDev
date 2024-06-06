7a6a87e0ca9c8ba2eb0c132f3380b9ec40daf8be
John Mutua <johnstone.m10@gmail.com>
2024-06-06 17:59:22 +0300

    finalized mathUtil file

--- javaScriptPractice2/mathUtil.js
+++ javaScriptPractice2/mathUtil.js
@@ -1,2 +1,13 @@
 const PI = 3.142;
 
+function getCircumference(radius) {
+    return 2 * PI * radius;
+}
+
+function getArea(radius) {
+    return PI * radius * radius;
+}
+
+function getVolume(radius) {
+    return 4 * PI * radius * radius;
+}
