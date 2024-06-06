51c9cab6d2ed1c2fb8d9ab9794f14e10ca15e201
John Mutua <johnstone.m10@gmail.com>
2024-06-06 18:00:08 +0300

    export keyword added

--- javaScriptPractice2/mathUtil.js
+++ javaScriptPractice2/mathUtil.js
@@ -1,13 +1,13 @@
-const PI = 3.142;
+export const PI = 3.142;
 
-function getCircumference(radius) {
+export  function getCircumference(radius) {
     return 2 * PI * radius;
 }
 
-function getArea(radius) {
+export  function getArea(radius) {
     return PI * radius * radius;
 }
 
-function getVolume(radius) {
+export function getVolume(radius) {
     return 4 * PI * radius * radius;
 }
