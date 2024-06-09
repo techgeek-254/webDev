9e0571c41230f24eb5b6a74ac39933c63526eea7
John Mutua <johnstone.m10@gmail.com>
2024-06-09 21:45:21 +0300

    some js added

---
+++ javaScriptCalculator/index.js
@@ -0,0 +1,15 @@
+//Calculator Program
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+
+}
+
+function calculate() {
+
+}
