942c5b0cfe7086a2e78b6a97cd58a04254ae79ad
John Mutua <johnstone.m10@gmail.com>
2024-06-17 12:05:31 +0300

    edits

--- toDoList_App2/app.js
+++ toDoList_App2/app.js
@@ -5,29 +5,30 @@ const listContainer = document.getElementById("list-container");
 
 function addTask() {
     if (inputBox.value === '') {
-        alert("You Must write Something!");
-        console.log("You Must write Something!");
+        alert("Please add a task !");
+        console.log("Please add a task !");
     }
     else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        //creating the delete task icon (x)
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
-    saveData()
+    saveData();
 }
 
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-        saveData()
+        saveData();
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveData()
+        saveData();
     }
 }, false);
 
